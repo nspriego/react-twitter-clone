80679122681331d7e8a2fcfc80d863e536e1f161
Nate Priego <nspriego@gmail.com>
2016-01-13 16:14:53 -0700

    Add components folder

---
+++ app/assets/frontend/components/TweetsList.jsx
@@ -0,0 +1,25 @@
+export default class TweetsList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul className="collection"> 
+          <li className="collection-item avatar">
+            <i className="material-icons circle">person_pin</i>
+            <span>Nate P</span>
+            <p>My #FirstTweet</p>
+          </li>
+          <li className="collection-item avatar">
+            <i className="material-icons circle">person_pin</i>
+            <span>Nate P</span>
+            <p>My #FirstTweet</p>
+          </li>
+          <li className="collection-item avatar">
+            <i className="material-icons circle">person_pin</i>
+            <span>Nate P</span>
+            <p>My #FirstTweet</p>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
--- app/assets/frontend/greet.jsx
+++
@@ -1,7 +0,0 @@
-export default class Greet extends React.Component {
-  render() {
-    return (
-      <h1>Greetings</h1>
-    );
-  }
-}
