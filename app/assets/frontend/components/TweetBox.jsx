80142dd2ad8d773515b2f13338eae0b08789ffd5
Nate Priego <nspriego@gmail.com>
2016-01-13 16:52:10 -0700

    Form submits and adds tweet to list

--- app/assets/frontend/components/TweetBox.jsx
+++ app/assets/frontend/components/TweetBox.jsx
@@ -1,10 +1,15 @@
 export default class TweetBox extends React.Component {
+  sendTweet(event) {
+    event.preventDefault();
+    this.props.sendTweet(this.refs.tweetTextArea.value);
+    this.refs.tweetTextArea.value = '';
+  }
   render() {
     return (
       <div className="row">
-        <form >
+        <form onSubmit={this.sendTweet.bind(this)}>
           <div className="input-field">
-            <textarea className="materialize-textarea" />
+            <textarea ref="tweetTextArea" className="materialize-textarea" />
             <label>What's Happening</label>
             <button className="btn right">Tweet</button>
           </div>
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,18 +1,22 @@
 import TweetBox from './components/TweetBox';
 import TweetsList from './components/TweetsList';
 
-let mockTweets = [
-  { id: 1, name: 'Nate P', body: 'My #FirstTweet'},
-  { id: 2, name: 'Nate P', body: 'My #SecondTweet'},
-  { id: 3, name: 'Nate P', body: 'My #ThirdTweet'},
-];
-
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tweetsList: [] };
+  }
+  addTweet(tweetToAdd) {
+    let newTweetsList = this.state.tweetsList;
+    newTweetsList.unshift({ id: Date.now(), name: 'Guest', body: tweetToAdd });
+
+    this.setState({tweetsList: newTweetsList});
+  }
   render() {
     return (
       <div className="container">
-        <TweetBox />
-        <TweetsList tweets={mockTweets} />
+        <TweetBox sendTweet={this.addTweet.bind(this)} />
+        <TweetsList tweets={this.state.tweetsList} />
       </div>
     );
   }
