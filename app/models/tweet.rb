308ac80abaf79d9a3b86ea9fc3688ac529f3a349
Nate Priego <nspriego@gmail.com>
2016-01-14 15:12:06 -0700

    Add gravatar

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,10 +2,15 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
 
   def as_json(opts={})
-    super(methods: [:name])
+    super(methods: [:name, :gravatar])
   end
 
   def name
     "#{user}"
+  end
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(user.email)
+    "http://www.gravatar.com/avatar/#{hash}"
   end
 end
