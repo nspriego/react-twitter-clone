8f0205bc807fb9ab3d62d1ef86637648ff652859
Nate Priego <nspriego@gmail.com>
2016-01-14 13:16:34 -0700

    Add User and User profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root 'home#index'
+
+  get 'profile' => 'profile#index'
+  patch 'profile' => 'profile#update'
+
+  # resources :profile, only: [:index, :update] do
+  #   get '/profile', to: 'profiles#index'
+  # end
 end
