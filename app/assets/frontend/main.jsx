120827c303becf2d86d34ed5b9b382b915488a4d
Nate Priego <nspriego@gmail.com>
2016-01-15 16:02:54 -0700

    Relocated Main component to the components folder and renamed to Index

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,44 +1,9 @@
-import TweetBox from './components/TweetBox';
-import TweetsList from './components/TweetsList';
-import TweetStore from "./stores/TweetStore";
-
-import TweetActions from "./actions/TweetActions";
-TweetActions.getAllTweets();
-
-let getAppState = () => {
-  return { tweetsList: TweetStore.getAll() };
-}
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = getAppState();
-    this._onChange = this._onChange.bind(this);
-  }
-  componentDidMount() {
-    TweetStore.addChangeListener(this._onChange);
-  }
-  componentWillUnmount() {
-    TweetStore.removeChangeListener(this._onChange);
-  }
-  _onChange() {
-    console.log(5, "Main._onChange")
-    this.setState(getAppState());
-  }
-  render() {
-    return (
-      <div className="container">
-        <TweetBox />
-        <TweetsList tweets={this.state.tweetsList} />
-      </div>
-    );
-  }
-}
+import Index from './components/Index'
 
 let documentReady = () => {
   let reactNode = document.getElementById('react');
   if (reactNode) {
-    ReactDOM.render( <Main />, reactNode);  
+    ReactDOM.render( <Index />, reactNode);  
   };
 };
 
